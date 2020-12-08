ad68a8970960e61d6130681e56375d7d1f80c119
Saikat <saikatmandal8@gmail.com>
2020-12-09 05:49:13 +0600

    added single fraction support in equation

--- Equation.hpp
+++ Equation.hpp
@@ -5,7 +5,7 @@
 #include<cmath>
 #include<string>
 
-//ONLY ADDITION AND SUBTRACTION ATM
+//ONLY ADDITION AND SUBTRACTION WITH SINGLE FRACTION ATM
 
 namespace Chronos {
     class Term {
@@ -26,9 +26,10 @@ namespace Chronos {
     class Equation {
         private:
         std::string m_equation;
-        std::vector<Term> m_variables;
-        void SetVariables(){
-            std::string pre_ope = "+", temp_eqn = m_equation + "=";
+        std::vector<std::vector<Term>> m_variables;
+        void SetVariables(std::string equation){
+            std::string pre_ope = "+", temp_eqn = equation + "=";
+            std::vector<Term> variables;
             for(int i = 0, start = 0; i < temp_eqn.size(); i++){
                 std::string variable;
                 if (temp_eqn[i] == '+' or temp_eqn[i] == '-' or temp_eqn[i] == '='){
@@ -42,7 +43,7 @@ namespace Chronos {
                                 degree = variable.substr(j+2);
                             if(j != 0)
                                 co_eff = variable.substr(0, j);
-                            m_variables.emplace_back(Term(std::stoi(co_eff), std::stoi(degree)));
+                            variables.emplace_back(Term(std::stoi(co_eff), std::stoi(degree)));
                             break;
                         }
                     }
@@ -58,10 +59,10 @@ namespace Chronos {
                         }
                         if(j == variable.size())
                             var.co_efficient = std::stoi(variable);
-                        m_variables.emplace_back(var);
+                        variables.emplace_back(var);
                     }
                     if(pre_ope == "-")
-                        m_variables.back().co_efficient *= -1;
+                        variables.back().co_efficient *= -1;
                     //Replace with index
                     if(temp_eqn[i] == '-')
                         pre_ope = "-";
@@ -69,24 +70,43 @@ namespace Chronos {
                         pre_ope = "+";
                 }
             }
+            m_variables.emplace_back(variables);
+        }
+        void CheckFraction(){
+            int length = m_equation.length();
+            bool fraction = false;
+            for(int i = 0; i < length; ++i){
+                if(m_equation[i] == '/'){
+                    SetVariables(m_equation.substr(1, i - 2));
+                    SetVariables(m_equation.substr(i + 2, (length - 1) - (i + 2)));
+                    fraction = true;
+                    break;
+                }
+            }
+            if(!fraction){
+                SetVariables(m_equation);
+                SetVariables("1");
+            }
         }
         public:
         Equation(const char* eqn) : m_equation(std::move(eqn)){
-            SetVariables();
+            CheckFraction();
         }
         Equation(std::string eqn) : m_equation(eqn){
-            SetVariables();
+            CheckFraction();
         }
 
         double Answer(double x){
-            double total = 0;
-            for(Term i : m_variables)
-                total += i * x;
-            return total;
+            double answer1 = 0, answer2 = 0;
+            for(Term i : m_variables[0])
+                answer1 += i * x;
+            for(Term i : m_variables[1])
+                answer2 += i * x;
+            return answer1 / answer2;
         }
 
         inline std::string GetEqn() { return m_equation; }
-        inline std::vector<Term> GetVariables() { return m_variables; }
+        inline std::vector<Term> GetVariables() { return m_variables[0]; }
     };
 }
 #endif
